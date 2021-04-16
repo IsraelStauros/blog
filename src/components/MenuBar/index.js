81b126623df2aea6916a5bf88cab06f35994ccd5
stauros <staurosisrael@gmail.com>
2021-04-16 17:30:37 -0300

    cleanup

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { HomeHeart as Home } from '@styled-icons/boxicons-solid/HomeHeart'
 import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular/SearchAlt2'
-import { UpArrowAlt as Arrow } from '@styled-icons/boxicons-regular/UpArrowAlt'
-import { Bulb as Light } from '@styled-icons/boxicons-regular/Bulb'
+import { UpArrowCircle as Arrow } from '@styled-icons/boxicons-regular/UpArrowCircle'
+import { Flash } from '@styled-icons/entypo/Flash'
 import { Grid } from '@styled-icons/boxicons-solid/Grid'
 
 import * as S from './styled'
@@ -21,7 +21,7 @@ const MenuBar = () => (
             </S.MenuBarLink>
         </S.MenuBarGroup>
         <S.MenuBarGroup>
-            <S.MenuBarItem title="Mudar Tema"><Light /></S.MenuBarItem>
+            <S.MenuBarItem title="Mudar Tema"><Flash /></S.MenuBarItem>
             <S.MenuBarItem title="Mudar Visualização"><Grid /></S.MenuBarItem>
             <S.MenuBarItem title="Ir ao Topo"><Arrow /></S.MenuBarItem>
         </S.MenuBarGroup>
