fb7937edbccfc3307a0ef3837489a2833f872557
stauros <staurosisrael@gmail.com>
2021-04-16 20:07:47 -0300

    styled

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -10,22 +10,30 @@ import * as S from './styled'
 
 const MenuBar = () => (
     <S.MenuBarWrapper>
-        <S.MenuBarGroup>
-            <S.MenuBarLink to="/" title="Voltar Para Home">
-                <S.MenuBarItem><Home /></S.MenuBarItem>
-            </S.MenuBarLink>
-        </S.MenuBarGroup>
-        <S.MenuBarGroup>
+      <S.MenuBarGroup>
+        <S.MenuBarLink to="/" title="Voltar para Home">
+          <S.MenuBarItem>
+            <Home />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
         <S.MenuBarLink to="/search/" title="Pesquisar">
-                <S.MenuBarItem><Search /></S.MenuBarItem>
-            </S.MenuBarLink>
-        </S.MenuBarGroup>
-        <S.MenuBarGroup>
-            <S.MenuBarItem title="Mudar Tema"><Flash /></S.MenuBarItem>
-            <S.MenuBarItem title="Mudar Visualização"><Grid /></S.MenuBarItem>
-            <S.MenuBarItem title="Ir ao Topo"><Arrow /></S.MenuBarItem>
-        </S.MenuBarGroup>
+          <S.MenuBarItem>
+            <Search />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+      </S.MenuBarGroup>
+      <S.MenuBarGroup>
+        <S.MenuBarItem title="Mudar o tema">
+          <Flash />
+        </S.MenuBarItem>
+        <S.MenuBarItem title="Mudar visualização">
+          <Grid />
+        </S.MenuBarItem>
+        <S.MenuBarItem title="Ir para o Topo">
+          <Arrow />
+        </S.MenuBarItem>
+      </S.MenuBarGroup>
     </S.MenuBarWrapper>
-)
-
-export default MenuBar
+  )
+  
+  export default MenuBar
