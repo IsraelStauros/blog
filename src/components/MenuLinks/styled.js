26945cb248da77d8b03e31e49618a166d91ba814
stauros <staurosisrael@gmail.com>
2021-04-16 14:12:31 -0300

    change styled

--- src/components/MenuLinks/styled.js
+++ src/components/MenuLinks/styled.js
@@ -16,7 +16,7 @@ export const MenuLinksItem = styled.li`
    }
 `
 
-export const MenuLinksLink = styled{Link}`
+export const MenuLinksLink = styled(Link)`
    color: #8899a6;
    text-decoration: none;
    transition: color 0.5s;
